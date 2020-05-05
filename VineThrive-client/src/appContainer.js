88d478e086cf8c09bf5965833d223e63ab0b9b73
Huda Yousif <hjoski@gmail.com>
2020-05-05 02:05:12 -0700

    add eventlistners to AppContainer

--- VineThrive-client/src/appContainer.js
+++ VineThrive-client/src/appContainer.js
@@ -4,14 +4,24 @@ class AppContainer {
     url = "http://localhost:3000"
     favoritePlants = {} 
 
+    bindEventListners(){
+        const btn = document.getElementById('createFavoritePlants');
+        btn.addEventListener('click', this.getFavotitePlants)
+    };
+
+    getFavotitePlants() {
+        this.constructor 
+    }
+
     getNotes() {
         // fetch reqeust to notes
+        console.log('something');
         fetch(this.url + '/notes')
         .then(resp => resp.json())
         .then(data => console.log(data))
         // populate the notes and plants properties and returned data 
         // Call renderNotes
-        .catch(err => alert(err))
+        .catch(err => alert(err));
     }
 
     renderNotes() {
