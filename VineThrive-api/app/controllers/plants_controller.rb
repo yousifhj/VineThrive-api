9207d64a65130ae167e576943a3c3b88371af4fd
Huda Yousif <hjoski@gmail.com>
2020-05-07 13:21:37 -0700

    created destroy method in Plants controller

--- VineThrive-api/app/controllers/plants_controller.rb
+++ VineThrive-api/app/controllers/plants_controller.rb
@@ -4,4 +4,10 @@ class PlantsController < ApplicationController
         render :json => Plant.all, :include => :category
     end 
 
+    def destroy
+        Plant.find(params[:id]).destroy
+        if plant.destroy
+            render json: { message: "Plant deleted"}
+        end
+    end 
 end
