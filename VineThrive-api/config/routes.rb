216e02b4b981dd609048db6a293d2cd0583e8e08
Huda Yousif <hjoski@gmail.com>
2020-05-07 13:21:12 -0700

    update Category class and routes

--- VineThrive-api/config/routes.rb
+++ VineThrive-api/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :categories, only: :index
-  resources :plants, only: :index
+  resources :plants, only: [:index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- VineThrive-client/src/category.js
+++ VineThrive-client/src/category.js
@@ -1,5 +1,6 @@
 class Category {
     constructor(name){
         this.name = name;
+        AppContainer.categories.push(this)
     }
 }
