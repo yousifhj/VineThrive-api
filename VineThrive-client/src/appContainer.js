c5250cd84cf94cc52fb3b432c468f9f56e7573e9
Huda Yousif <hjoski@gmail.com>
2020-05-06 21:11:32 -0700

    insiantiate and insert data app container

--- VineThrive-client/src/appContainer.js
+++ VineThrive-client/src/appContainer.js
@@ -1,37 +1,53 @@
 class AppContainer {
-    plants = []
-    categories = []
+    static plants = [];
+    static categories = [];
     url = "http://localhost:3000"
-    RandomPlants = {} 
+    static dailyPlants = {};
 
     bindEventListners(){
-        const btn = document.getElementById('createRandomPlants');
-        btn.addEventListener('click', this.getFavotitePlants.bind(this))
-    };
-
-    getFavotitePlants() {
-        let randomPlants = [];
-        for (let i = 0; i < 4; i++) {
-            debugger
-            randomPlants.push(this.plants[Math.floor(Math.random()*this.plants.length)]);
-        };
-
-        return randomPlants;
+        const btn = document.getElementById('createDailyPlants');
+        btn.addEventListener('click', this.getRandomPlants);
     }
 
+    getRandomPlants() {
+        console.log('hello')
+        let randomPlants = [];
+        AppContainer.categories.forEach(category => {
+            randomPlants.push(Plants.byCategory(category.name)[Math.floor(Math.random()*AppContainer.plants.length)]);
+        });
+        // insiantiate a DailyPlants instance with these Plants
+        new DailyPlants(randomPlants)
+        // insert data into DOM
+        const dailyPlantsDiv = document.getElementById('dailyPlants');
+        AppContainer.dailyPlants.plants.forEach(dailyPlants => {
+            const plantDiv = document.createElement('div');
+            plantDiv.innerText = dailyPlants.name;
+            dailyPlantsDiv.appendChild(plantDiv);
+        })
+    }
 
     getPlants() {
-        // fetch reqeust to plants
-        console.log('something');
+        // fetch reqeust to /plants
+                console.log('something');
         fetch(this.url + '/plants')
         .then(resp => resp.json())
-        .then(data => console.log(data))
         // populate the plants and plants properties and returned data 
-        // Call renderPlants
+        .then(data => {
+            console.log(data)
+            data.forEach(plant => {
+                new Plant(plant.name, plant.category)
+                if (!AppContainer.categories.map(category => category.name).includes(plant.category.name)) {
+                    new Category(plant.category.name)
+                }
+            });
+            // Call renderPlants
+            this.renderPlants();
+        })
         .catch(err => alert(err));
-    }
+    };
 
     renderPlants() {
         // create DOM nodes and indert data into and render DOM
+
     }
  }
