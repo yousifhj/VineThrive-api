c0edee7b963b313dfafb4fbbf41e112c8eb090f0
Huda Yousif <hjoski@gmail.com>
2020-05-08 22:24:11 -0700

    update routes and categories

--- VineThrive-api/app/models/category.rb
+++ VineThrive-api/app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-    has_many :plant
+    has_many :plants
 end
--- VineThrive-api/config/routes.rb
+++ VineThrive-api/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :categories, only: :index
-  resources :plants, only: [:index, :destroy, :create]
+  # resources :categories, only: :index
+  # resources :plants, only: [:index, :destroy, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :categories do 
+    resources :plants, only: [:index, :destroy, :create]
+  end 
+
+  resources :plants, only: [:index, :destroy]
 end
+
+
