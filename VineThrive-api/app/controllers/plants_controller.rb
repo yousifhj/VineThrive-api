5c65c1e92f867f581043f3bcc5bb0a39db2015e9
Huda Yousif <hjoski@gmail.com>
2020-05-18 15:10:05 -0700

    update plants controller

--- VineThrive-api/app/controllers/plants_controller.rb
+++ VineThrive-api/app/controllers/plants_controller.rb
@@ -5,24 +5,18 @@ class PlantsController < ApplicationController
     end 
 
     def create
-        if params[:category_id]
-            category = Category.find(params[:category_id])
-            plant = category.plants.build(plant_params)
-            if plant.save 
-                render :json => plant, :include => :category
-            end
-        end 
 
-        # category = Category.find_by(name: params[:category])
-        # plant = Plant.create(name: params[:name], category: category)
-        # render :json => plant, :include => :category
-
-        # plant = Plant.new(plant_params)
-        # if plant.save 
-        #     render json: plant, status: :created
-        # else 
-        #     render json: plant.errors, status: :unprocessable_entity
-        # end
+        name = params[:name]
+        category_name = params[:category]
+        category = Category.where(name: category_name.downcase).take
+        if category.present?
+            plant = category.plants.create({
+                name: name
+            })
+            puts plant.to_json
+        end
+         
+        render json: plant
     end 
 
     def destroy
