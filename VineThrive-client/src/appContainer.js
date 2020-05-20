300505aedc62554bc45e0681d9cd00da706a4364
Huda Yousif <hjoski@gmail.com>
2020-05-19 22:46:31 -0700

    add CSS

--- VineThrive-client/src/appContainer.js
+++ VineThrive-client/src/appContainer.js
@@ -1,27 +1,23 @@
+
 class AppContainer {
+    
     static plants = [];
     static categories = [];
      url = "http://localhost:3000"
     static dailyPlants = {};
 
-    bindEventListners(){
-        const btn = document.getElementById('createDailyPlants');
-        btn.addEventListener('click', () => this.getRandomPlants());
-
-        const newPlantForm = document.getElementById('newPlant');
-        newPlantForm.addEventListener('submit', () => this.createPlant(event));
+    
+    bindEventListeners() {
+        AppListener.createPlantsListener();
+        AppListener.createDailyPlantsListener();
+        AppListener.getPlantListeners();
     }
 
     // this = instace of app container  
     createPlant(event) {
         event.preventDefault();
-        // const form = document.getElementById('newPlant')
         const data = event.target;
-        console.log(JSON.stringify({
-            name: data.plant.value,
-            category: data.children[2].value
-          }))
-        
+
         fetch(`${this.url}/categories/plants`, {
             method: 'POST',
             headers: {
@@ -30,25 +26,26 @@ class AppContainer {
             },
             body: JSON.stringify({
               name: data.plant.value,
-              category: data.children[2].value
+              category: document.getElementById('categorySelect').value
             })
         })
         .then(resp => {
             document.getElementById("plant_name_input").value = ""
-        }).catch(err => console.log(err))
-        .then(() => {
+        }).catch(err => {
             console.log('done')
-        });
+        })
     }
 
     getRandomPlants() {
         let randomPlants = [];
+        
         AppContainer.categories.forEach(category => {
+            console.log(category)
             randomPlants.push(Plant.byCategory(category.name)[Math.floor(Math.random()*Plant.byCategory(category.name).length)]);
         });
         // insiantiate DailyPlants with these Plants
         new DailyPlants(randomPlants)
-        // insert data into DOM
+
         AppContainer.dailyPlants.plants.forEach(plant => {
             const container = document.querySelector('.container');
             const dailyPlantsDiv = document.getElementById('dailyPlants');
@@ -62,45 +59,43 @@ class AppContainer {
             plantDiv.setAttribute("id", plantId)
 
             const remove = document.createElement('button');
-            remove.classList.add('remove');
+            remove.classList.add('remove', 'btn', 'btn-danger', 'btn-sm', 'float-right');
             remove.setAttribute("id", plantId);
             remove.innerHTML = "REMOVE";
             remove.addEventListener ('click', () => {this.removePlant(plantId)});
-            
             container.appendChild(dailyPlantsDiv);
             dailyPlantsDiv.appendChild(plantDiv);
             plantDiv.appendChild(remove);
         })
-
     }
 
-    removePlant (id) {
+    removePlant(id) {
         fetch(`${this.url}/plants/${id}`, {
-            method: 'DELETE'
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
         }).then(res => {
             console.log('success remove', id)
             let elem = document.getElementById(id)
             elem.remove()
          }).catch(err => {
             console.log(err)
-    
         })
     }
 
     getPlants() {
-        // fetch reqeust to /plants
         fetch(this.url + '/plants')
         .then(resp => resp.json())
-        // populate the plants and plants properties and returned data 
         .then(data => {
-            console.log(data)
             data.forEach(plant => {
                 new Plant(plant.id, plant.name, plant.category)
                 if (!AppContainer.categories.map(category => category.name).includes(plant.category.name)) {
-                   new Category(plant.category.name)
+                    new Category(plant.category.name)
                 }
             });
-            // Call renderPlants
+
             this.renderPlants();
         })
         .catch(err => alert(err));
@@ -108,23 +103,28 @@ class AppContainer {
 
     renderPlants() {
         // create DOM nodes and indert data into and render DOM 
-        
-        const viningPlantsSelect = document.getElementById('viningPlants');
-        const easyCareSelect = document.getElementById('easyCare');
-        const bigIndoorSelect = document.getElementById('bigIndoor');
+        const viningPlantsDiv = document.getElementById('viningPlants');
+        const easyCareDiv = document.getElementById('easyCare');
+        const bigIndoorDiv = document.getElementById('bigIndoor');
+        viningPlantsDiv.innerHTML = "";
+        easyCareDiv.innerHTML = "";
+        bigIndoorDiv.innerHTML = "";
+
         AppContainer.plants.forEach(plant => {
-            const option = document.createElement('option');
-            option.innerText = plant.name;
-            // where we append will be conditional based on what category it belongs to
+            console.log(plant)
+            const p = document.createElement('p');
+            p.innerText = plant.name;
+            console.log(plant.category.name)
+            // append will be conditional based on what category it belongs to
             switch (plant.category.name) {
                 case "viningplants":
-                    viningPlantsSelect.appendChild(option);
+                    viningPlantsDiv.appendChild(p);
                     break;
                 case "easycare":
-                    easyCareSelect.appendChild(option);
+                    easyCareDiv.appendChild(p);
                     break;
                 case "bigindoor":
-                    bigIndoorSelect.appendChild(option);
+                    bigIndoorDiv.appendChild(p);
                     break;
                 default:
             }
